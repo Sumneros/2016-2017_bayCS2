2780a6dff193a6f543a5e3826bde4739c7ae9771
Richard Piccioni <rpiccioni@bayschoolsf.org>
2016-05-11 16:11:12 +0000

    fixed is_prime.  Thanks, David!

--- 2015cs61a/disc02/drp_disc02.py
+++ 2015cs61a/disc02/drp_disc02.py
@@ -46,15 +46,30 @@ def expt(base, power):
 
 def is_prime(n):
     """
-    >>> is_prime(25)
-    3
-    5
-    7
-    11
-    13
-    17
-    19
-    23
+    >>> is_prime(0)
+    False
+    >>> is_prime(1)
+    False
+    >>> is_prime(2)
+    False
+    >>> is_prime(3)
+    True
+    >>> is_prime(4)
+    False
+    >>> is_prime(5)
+    True
+    >>> is_prime(6)
+    False
+    >>> is_prime(7)
+    True
+    >>> is_prime(8)
+    False
+    >>> is_prime(9)
+    False
+    >>> is_prime(10)
+    False
+    >>> is_prime(11)
+    True
     """
 # YOUR CODE HERE #
 
